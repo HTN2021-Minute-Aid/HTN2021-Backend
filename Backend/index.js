84dc1aff378d48b6843cdf8812d00c45b365cd3d
Nathan Wong <56657800+NathanWong1106@users.noreply.github.com>
2021-01-17 02:19:56 -0500

    2

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const FirebaseHandler = require('./firebaseHandler.js')
 const express = require('express')
 const bodyParser  = require('body-parser');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000;
 const app = express()
-app.use(express.json())
+
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
 
 app.post("/transcripts/add", async function(req, res){
     const response = await FirebaseHandler.addTranscript(req)
